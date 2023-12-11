bb7e9afa1bc34c26de5f0a877c11bf114e07696b
kereszteshunor <kereszteshunor36@gmail.com>
2023-12-11 09:20:58 +0100

    karakter osztaly kibovitve

--- src/javajatek/Karakter.java
+++ src/javajatek/Karakter.java
@@ -1,12 +1,81 @@
 package javajatek;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Karakter
 {
-    private ArrayList<String> felszereles;
+    private final ArrayList<Targy> felszereles;
     
     private int eletero;
     private int ugyesseg;
     private int szerencse;
+    
+    private static final Random r = new Random();
+
+    public Karakter()
+    {
+        felszereles = new ArrayList<>();
+        eletero = d6(2) + 12;
+        ugyesseg = d6() + 6;
+        szerencse = d6() + 6;
+    }
+
+    public int getEletero()
+    {
+        return eletero;
+    }
+
+    public void setEletero(int eletero)
+    {
+        this.eletero = eletero;
+    }
+
+    public int getUgyesseg()
+    {
+        return ugyesseg;
+    }
+
+    public void setUgyesseg(int ugyesseg)
+    {
+        this.ugyesseg = ugyesseg;
+    }
+
+    public int getSzerencse()
+    {
+        return szerencse;
+    }
+
+    public void setSzerencse(int szerencse)
+    {
+        this.szerencse = szerencse;
+    }
+    
+    public void felvesz(Targy t)
+    {
+        felvesz(t, 1);
+    }
+    
+    public void felvesz(Targy t, int db)
+    {
+        for (int i = 0; i < db; i++)
+        {
+            felszereles.add(t);
+        }
+    }
+    
+    private static int d6()
+    {
+        return d6(1);
+    }
+    
+    private static int d6(final int n)
+    {
+        int eredmeny = 0;
+        for (int i = 0; i < n; i++)
+        {
+            eredmeny = r.nextInt(6) + 1;
+        }
+        return eredmeny;
+    }
 }
---
+++ src/javajatek/Targy.java
@@ -0,0 +1,38 @@
+package javajatek;
+
+import java.util.UUID;
+
+public class Targy
+{
+    private final String nev;
+    private final String leiras;
+    
+    private final UUID id;
+
+    public Targy(String nev, String leiras)
+    {
+        this.nev = nev;
+        this.leiras = leiras;
+        id = UUID.randomUUID();
+    }
+
+    public String getNev()
+    {
+        return nev;
+    }
+
+    public String getLeiras()
+    {
+        return leiras;
+    }
+
+    public UUID getId()
+    {
+        return id;
+    }
+
+    @Override public String toString()
+    {
+        return nev + ":\n" + leiras + "\n";
+    }
+}
